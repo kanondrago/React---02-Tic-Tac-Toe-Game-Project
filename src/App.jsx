6818b60a022cec7ce1043d7fdf53553bf1f2147e
wolfrit <wolfrit.company@gmail.com>
2025-09-02 23:17:55 +0200

    Primer commit of the game

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+function App() {
+  
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="Player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="Player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+
+        GAME BOARD
+      </div>
+
+      LOGS
+    </main>
+  )
+}
+
+export default App
