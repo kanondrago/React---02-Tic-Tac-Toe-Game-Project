d4f58ccb9d5a054f9cff76323369299c8ed91109
wolfrit <wolfrit.company@gmail.com>
2025-09-07 17:12:50 +0200

    object or an Array that are tecnically Objects in JS

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 const initialGameBoard = [
     [null, null, null], 
@@ -7,13 +8,26 @@ const initialGameBoard = [
 
 export default function GameBoard() {
 
+    // Carefull to change the state of an object or an Array that are tecnically Objects in JS.
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex, symbol) {
+        setGameBoard((prevGameBoard) => {
+            // Approach and pattern
+            // Using this, we are updating the state in an immutable way => ... Is strongly recommended...
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = symbol
+            return updatedBoard;
+        })
+    }
+
 
     return (
         <ol id="game-board">
             {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, playerSymbolIndex) => <li key={playerSymbolIndex}>
-                        <button>
+                        <button onClick={handleSelectSquare}>
                             {playerSymbol}
                         </button>
                     </li>)}
