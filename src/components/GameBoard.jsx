023c0d219040cad174043d85c549f24e2a46ff6c
wolfrit <wolfrit.company@gmail.com>
2025-09-07 17:32:36 +0200

    learning about objects in JS and send parameters from the tags

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -10,8 +10,12 @@ export default function GameBoard() {
 
     // Carefull to change the state of an object or an Array that are tecnically Objects in JS.
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    const [turn, setTurn] = useState(true);
 
     function handleSelectSquare(rowIndex, colIndex, symbol) {
+
+        setTurn((t) => !t);
+
         setGameBoard((prevGameBoard) => {
             // Approach and pattern
             // Using this, we are updating the state in an immutable way => ... Is strongly recommended...
@@ -24,10 +28,10 @@ export default function GameBoard() {
 
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, playerSymbolIndex) => <li key={playerSymbolIndex}>
-                        <button onClick={handleSelectSquare}>
+                    {row.map((playerSymbol, colIndex) => <li key={colIndex}>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex, turn?'X':'O')}>
                             {playerSymbol}
                         </button>
                     </li>)}
