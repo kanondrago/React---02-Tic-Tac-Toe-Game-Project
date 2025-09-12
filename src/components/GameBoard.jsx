51157780d6271f8886376b9ba12e7330cf8560f0
wolfrit <wolfrit.company@gmail.com>
2025-09-12 14:41:32 +0200

    avoid to press the same button selection

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -23,7 +23,7 @@ export default function GameBoard({onSelectSquare, turns}) {
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol!==null}>
                             {playerSymbol}
                         </button>
                     </li>)}
